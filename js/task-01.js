f82a60a28131a705c2e263cd056c58d05f18fcdc
Anastasiia2827 <nasty.bond2016@gmail.com>
2022-12-09 20:23:54 +0200

    1 task

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+const findCategory = document.querySelector("#categories");
+console.log('Number of categories:', findCategory.childElementCount);
+
+const newArray = [...findCategory.children];
+newArray.forEach((element) => {
+    console.log("Category:",
+        element.firstElementChild.textContent);
+    console.log("Elements:", element.lastElementChild.childElementCount);
+});
--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemElPotatoes = document.createElement('li');
+itemElPotatoes.classList.add('item');
+itemElPotatoes.textContent = 'Potatoes'; 
+console.log(itemElPotatoes);
+
+const itemElMushrooms = document.createElement('li');
+itemElMushrooms.classList.add('item');
+itemElMushrooms.textContent = 'Mushrooms'; 
+console.log(itemElMushrooms);
+
+ingredients.append(itemElPotatoes, itemElMushrooms);
+console.log(ingredients);
+
+
