e2e754c6c6b0da28cbe42908b0fcc6bb12edde7f
Anastasiia2827 <nasty.bond2016@gmail.com>
2022-12-14 00:27:25 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit',onFormSubmit );
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const formEl = evt.currentTarget.elements;
+
+    if (!formEl.email.value || !formEl.password.value) {
+        alert('Всі поля повинні бути заповнені');
+    }
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+
+    const formData = {
+        email,
+        password,
+    };
+    console.log(formData);
+    form.reset();
+}
+
 
