0475c52e9538dfef85664bb7b7c3b2b711869da6
Anastasiia2827 <nasty.bond2016@gmail.com>
2022-12-14 22:04:23 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
+const buttonRandomColor = document.querySelector('.change-color');
+const backgroundColor = document.querySelector('.color');
+
+buttonRandomColor.addEventListener('click', () => {
+  document.body.style.backgroundColor = `${getRandomHexColor()}`;
+  backgroundColor.textContent = `${getRandomHexColor()}`;
+})
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
