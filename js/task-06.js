88e2e508b31893cd5cb447b3e158c72bdce71b39
Anastasiia2827 <nasty.bond2016@gmail.com>
2022-12-15 21:25:08 +0200

    правки до task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,11 @@
-input = document.querySelector('[data-length="6"]');
+const input = document.querySelector('[data-length="6"]');
 
 input.addEventListener('blur', onInputBlur);
 
 function onInputBlur(evt) {
     input.classList.add('invalid');
     input.classList.remove('valid');
-    if (evt.currentTarget.lenght == input.dataset.length) {
+    if (evt.currentTarget.value.length == input.dataset.length) {
       input.classList.remove('invalid');
       input.classList.add('valid');
     }
