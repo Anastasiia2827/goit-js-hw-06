b845f433b56e261e5b0b4d492996e4adce2ef794
Anastasiia2827 <nasty.bond2016@gmail.com>
2022-12-19 17:20:40 +0200

    task6-changes
    зміни після зауважень ментора

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,26 @@
+// const input = document.querySelector('[data-length="6"]');
+
+// input.addEventListener('blur', onInputBlur);
+
+// function onInputBlur(evt) {
+//     input.classList.add('invalid');
+//     input.classList.remove('valid');
+//     if (evt.currentTarget.value.length == input.dataset.length) {
+//       input.classList.remove('invalid');
+//       input.classList.add('valid');
+//     }
+// }
+
 const input = document.querySelector('[data-length="6"]');
-
 input.addEventListener('blur', onInputBlur);
-
+ 
 function onInputBlur(evt) {
-    input.classList.add('invalid');
-    input.classList.remove('valid');
-    if (evt.currentTarget.value.length == input.dataset.length) {
-      input.classList.remove('invalid');
-      input.classList.add('valid');
+    changeClass(evt.target, 'invalid', 'valid')
+    if (evt.currentTarget.value.length === Number(input.dataset.length)) {
+       changeClass(evt.target, 'valid', 'invalid')
     }
+ }
+function changeClass(ref, add, rem) {
+   ref.classList.add(add)
+   ref.classList.remove(rem)
 }
