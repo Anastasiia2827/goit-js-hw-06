4ae730198eb963fe6b543e46493a0af305732775
Anastasiia2827 <nasty.bond2016@gmail.com>
2022-12-19 14:50:43 +0200

    task9-changes
    зміни після зауваження ментора

--- js/task-09.js
+++ js/task-09.js
@@ -1,10 +1,22 @@
-const buttonRandomColor = document.querySelector('.change-color');
-const backgroundColor = document.querySelector('.color');
+// const buttonRandomColor = document.querySelector('.change-color');
+// const backgroundColor = document.querySelector('.color');
 
-buttonRandomColor.addEventListener('click', () => {
-  document.body.style.backgroundColor = `${getRandomHexColor()}`;
-  backgroundColor.textContent = `${getRandomHexColor()}`;
-})
+// buttonRandomColor.addEventListener('click', () => {
+//   document.body.style.backgroundColor = `${getRandomHexColor()}`;
+//   backgroundColor.textContent = `${getRandomHexColor()}`;
+// })
+
+const buttonRandomColor = document.querySelector('.change-color');
+const spanBackgroundColor = document.querySelector('.color');
+const body = document.querySelector("body");
+
+function buttonRandomColorClic() {
+  const randomColor = getRandomHexColor();
+  spanBackgroundColor.textContent = randomColor;
+  body.style.backgroundColor = randomColor;
+}
+
+buttonRandomColor.addEventListener("click", buttonRandomColorClic)
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
